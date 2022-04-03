c934c721f53a8ebb9883e53598383e3d16269e3b
Joren <Joren.dom@outlook.com>
2022-04-04 01:19:36 +0200

    add colors, and a random option

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 
-
+const amount = document.getElementById("amount")
+let color ="Black"
 
 function createGrid(amount) {
+
     let container = document.querySelector(".container");
     container.style.gridTemplateRows =`repeat(${amount}, 1fr)`;
     container.style.gridTemplateColumns =`repeat(${amount}, 1fr)`;
 
-    for(let c = 0; c < (256* 2); c++){
-         let cell = document.createElement("div");
-        cell.style.backgroundColor ="black"
+    for(let c = 0; c < (amount * amount); c++){
+        let cell = document.createElement("div1");
+        cell.addEventListener("mouseover", cellColor);
+        cell.style.backgroundColor ="white";
         container.insertAdjacentElement("beforeend" ,cell);
-    
-        }
+    }
 }
 
-function resetGrid(){
-
+function changeAmount(amount){
+    if(amount > 150 || amount < 0){
+        alert(`Number too high or negative :${amount}`);
+    }
+    else{
+        createGrid(amount);
+    }
 }
+
+function cellColor(){ 
+    if(color === 'random'){
+        this.style.backgroundColor = `hsl(${Math.random()* 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = color;
+}
+}
+
+function changeColor(choice){
+    color = choice;
+}
+
+function resetGrid(cell){
+    changeAmount(16)
+    cell = document.querySelectorAll("div1");
+    cell.forEach(div => div.remove());
+};
  
